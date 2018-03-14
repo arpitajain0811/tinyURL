ce84f0f0a019f3488413370c2e6358b5d6bf4a37
Arpita <arpitajain0811@gmail.com>
2018-03-14 09:52:05 +0530

    modifying model

--- migrations/20180305045239-create-tinyurl.js
+++ migrations/20180305045239-create-tinyurl.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('tinyurls', {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('tinyurl', {
     id: {
       allowNull: false,
       autoIncrement: true,
@@ -24,5 +24,5 @@ module.exports = {
       type: Sequelize.DATE,
     },
   }),
-  down: (queryInterface, Sequelize) => queryInterface.dropTable('tinyurls'),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('tinyurl'),
 };
